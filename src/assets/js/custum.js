ff486b4d364b8d9b260bba9a884be7bb4ef9d29a
ismael <7ilipe@gmail.com>
2020-09-08 00:10:15 +0100

    ultima versao

---
+++ src/assets/js/custum.js
@@ -0,0 +1,12 @@
+/**
+ * @File   : custum.js
+ * @Author : Gunza Ismael (7ilipe@gmail.com)
+ * @Link   : 
+ * @Date   : 07/08/2020, 02:41:45
+ */
+
+$(document).ready(function() {
+
+    $('.carousel').carousel();
+
+});
